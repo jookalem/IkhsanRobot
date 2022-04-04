b8b8c2787c708f0fea33e2ab2e65ac5f41ce4c86
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-04-05 01:14:28 +0700

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -83,7 +83,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-SIESTA_IMG = "https://telegra.ph/file/6d3e3ef24f64fb6e35df0.jpg"
+SIESTA_IMG = "https://telegra.ph/file/33b4ea70db34797f53007.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
  You can support the project by contacting @ikhsanntarjo \
